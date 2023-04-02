7f8ab3b74b1ff7584f5db6724cc1962efc2b088a
mjarmak <mohamadjarmak@gmail.com>
2023-04-02 12:59:31 +0200

    lombok

--- src/main/java/guru/springframework/spring6di/services/EnvironmentServiceDev.java
+++ src/main/java/guru/springframework/spring6di/services/EnvironmentServiceDev.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6di.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Service;
  * Created by jt, Spring Framework Guru.
  */
 @Profile({"dev", "default"})
-@Service
+@Service // needed for automatic dependency injection
 public class EnvironmentServiceDev implements EnvironmentService {
     @Override
     public String getEnv() {
